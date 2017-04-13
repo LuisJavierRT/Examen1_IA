816690affd6fd119b5ca19a86412c2fd9d5fc1fe
LuisJavierRT <lujaramireztorres@gmail.com>
2017-04-12 18:20:20 -0600

    Commit 2

--- 1examen_IA/logic.js
+++ 1examen_IA/logic.js
@@ -4,6 +4,9 @@ var idNode = undefined;
 var idEdges = [];
 var data = {};
 var network = undefined;
+var container = undefined;
+var options = undefined;
+var logicNetwork = [];
 
 var auto_graph = function(number){
     var listaNodes = [];
@@ -19,35 +22,39 @@ var auto_graph = function(number){
         listaNodes.push(node);
     }
     var nodes = new vis.DataSet(listaNodes);
-    
 
     for (var i = 1; i <= n; i++) {
-        var m = randomNumber(7,0);
-        for (var j = 1; j<= m; j++) {
-            var x = randomNumber(n,1);
+        var m = randomNumber(5,0);
+        if(m>n) {
+            m = n;
+        }
+        var uniques = chance.unique(chance.natural, m, {min: 1, max: n});
+        var listaEd = [];
+
+        for (var j = 1; j<= uniques.length; j++) {
             var val = randomNumber(10,1);
             var edge = {
                 from: i,
-                to: x,
+                to: uniques[j],
                 label: val,
                 color: "blue"
-                
             };
             listaEdges.push(edge);
+            listaEd.push({to: uniques[j], weigth: val});
         }
+        logicNetwork.push({id: i, visited: false, edges: listaEd});
     }
     var edges = new vis.DataSet(listaEdges);
-    console.log(edges);
     // create a network
-    var container = document.getElementById('mynetwork');
+    container = document.getElementById('mynetwork');
 
     // provide the data in the vis format
-     data = {
+    data = {
         nodes: nodes,
         edges: edges
     };
     
-    var options = {};
+    options = {layout: {improvedLayout: false}};
     // initialize your network!
     network = new vis.Network(container, data, options);
 
@@ -61,12 +68,28 @@ var randomNumber = function(n1,n2){
     return Math.floor(Math.random()*n1)+n2;
 };
 
-auto_graph(100);
+auto_graph(50);
 
 var deleteNode = function(){
     if(idNode != undefined){
         data.nodes.remove({id: idNode});
+        updateLogicNetwork(idNode);
         idNode = undefined;
+    }
+};
+
+var updateLogicNetwork = function(id) {
+    for(var i=0; i<logicNetwork.length; i++) {
+        if(logicNetwork[i].id == id) {
+            logicNetwork.splice(i, 1);
+        }
+        else{
+            for(var j=0; j<logicNetwork[i].edges.length; j++) {
+                if(logicNetwork[i].edges[j].to == id) {
+                    logicNetwork[i].edges.splice(j, 1);
+                }
+            }
+        }
     }
 };
 
@@ -75,8 +98,7 @@ var setInitialState = function(){
        data.nodes.update({id: idNode, initial: true, color: "#52D408"});
        for (var i = 0 ; i <= idEdges.length; i++) {
            data.edges.update({id: idEdges[i], color: "blue"});
-       }
-       idNode = undefined;   
+       }    
     }
 };
 
@@ -85,7 +107,71 @@ var setFinalState = function(){
         data.nodes.update({id: idNode, final: true, color: "#FAE409"});
         for (var i = 0 ; i <= idEdges.length; i++) {
            data.edges.update({id: idEdges[i], color: "blue"});
-       }
-        idNode = undefined;     
+       }    
     }
 };
+
+
+var saveText = function(text, filename){
+  var a = document.createElement('a');
+  a.setAttribute('href', 'data:text/plain;charset=utf-u,'+encodeURIComponent(text));
+  a.setAttribute('download', filename);
+  a.click();
+};
+
+var saveGraphJSON = function(){
+    saveText(JSON.stringify(data), "filename.json" );
+};
+
+
+var json;
+
+function handleFileSelect(evt,callback) {
+
+    var files = evt.target.files; // FileList object
+
+    // files is a FileList of File objects. List some properties.
+    var output = [];
+    for (var i = 0, f; f = files[i]; i++) {
+        var reader = new FileReader();
+
+        // Closure to capture the file information.
+        reader.onloadend = (function (theFile) {
+            return function (e) {
+                    try {
+
+                        callback(e.target.result);
+                       
+                    } catch (ex) {
+                        alert('ex when trying to parse json = ' + ex);
+                    }
+            }
+        })(f);
+
+        reader.readAsText(f);
+    }
+};
+
+function loadGraphJSON(evt){
+    handleFileSelect(evt,function(result){
+
+        json = JSON.parse(result);
+
+        var arr = Object.values(json.nodes._data);
+        var arr2 = Object.values(json.edges._data);
+        
+        var nodos = new vis.DataSet(arr);
+        var arcos = new vis.DataSet(arr2);
+        var data2 = {
+            nodes: nodos,
+            edges: arcos
+        };
+
+        network = new vis.Network(container, data2, options);
+
+        network.redraw();
+    });
+};
+
+document.getElementById('files').addEventListener('change', loadGraphJSON, false);
+
