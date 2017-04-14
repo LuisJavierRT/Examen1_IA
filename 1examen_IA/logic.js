7f9eca92660e0d859025f74d8fe1bf3e6d0cc2a1
josehidalgo8396 <josehidalgo8396@gmail.com>
2017-04-13 22:52:35 -0600

    2 algoritmos implementados (DFS y DLS)

--- 1examen_IA/logic.js
+++ 1examen_IA/logic.js
@@ -7,6 +7,7 @@ var network = undefined;
 var container = undefined;
 var options = undefined;
 var logicNetwork = [];
+var json;
 
 var auto_graph = function(number){
     var listaNodes = [];
@@ -17,7 +18,8 @@ var auto_graph = function(number){
             id: i,
             label: abc[i-1],
             initial: false,
-            final: false
+            final: false,
+            visited: false
         };
         listaNodes.push(node);
     }
@@ -31,7 +33,7 @@ var auto_graph = function(number){
         var uniques = chance.unique(chance.natural, m, {min: 1, max: n});
         var listaEd = [];
 
-        for (var j = 1; j<= uniques.length; j++) {
+        for (var j = 0; j < uniques.length; j++) {
             var val = randomNumber(10,1);
             var edge = {
                 from: i,
@@ -42,8 +44,9 @@ var auto_graph = function(number){
             listaEdges.push(edge);
             listaEd.push({to: uniques[j], weigth: val});
         }
-        logicNetwork.push({id: i, visited: false, edges: listaEd});
+        logicNetwork.push({id: i, name: abc[i-1], visited: false, final: false, initial: false, edges: listaEd});
     }
+    console.log(logicNetwork);
     var edges = new vis.DataSet(listaEdges);
     // create a network
     container = document.getElementById('mynetwork');
@@ -68,7 +71,7 @@ var randomNumber = function(n1,n2){
     return Math.floor(Math.random()*n1)+n2;
 };
 
-auto_graph(50);
+auto_graph(10);
 
 var deleteNode = function(){
     if(idNode != undefined){
@@ -95,10 +98,16 @@ var updateLogicNetwork = function(id) {
 
 var setInitialState = function(){
     if(idNode != undefined){
-       data.nodes.update({id: idNode, initial: true, color: "#52D408"});
-       for (var i = 0 ; i <= idEdges.length; i++) {
+        data.nodes.update({id: idNode, initial: true, color: "#52D408"});
+        for (var i = 0 ; i < idEdges.length; i++) {
            data.edges.update({id: idEdges[i], color: "blue"});
-       }    
+        }   
+        for (var i=0; i<logicNetwork.length; i++) {
+            if(logicNetwork[i].id == idNode) {
+                logicNetwork[i].initial = true;
+                break;
+            }
+        } 
     }
 };
 
@@ -107,9 +116,23 @@ var setFinalState = function(){
         data.nodes.update({id: idNode, final: true, color: "#FAE409"});
         for (var i = 0 ; i <= idEdges.length; i++) {
            data.edges.update({id: idEdges[i], color: "blue"});
+        }
+        for (var i=0; i<logicNetwork.length; i++) {
+            if(logicNetwork[i].id == idNode) {
+                logicNetwork[i].final = true;
+                break;
+            }
        }    
     }
 };
+
+var clearAllStates = function() {
+    for(var i=0; i<logicNetwork.length; i++) {
+        logicNetwork[i].initial = false;
+        logicNetwork[i].final = false;
+        data.nodes.update({id: i+1, initial: false, final: false, color:"#99ccff"});
+    }
+}
 
 
 var saveText = function(text, filename){
@@ -122,9 +145,6 @@ var saveText = function(text, filename){
 var saveGraphJSON = function(){
     saveText(JSON.stringify(data), "filename.json" );
 };
-
-
-var json;
 
 function handleFileSelect(evt,callback) {
 
@@ -171,6 +191,100 @@ function loadGraphJSON(evt){
 
         network.redraw();
     });
+};
+
+var DFS = function(graph, node1, goal) {
+    var stack = [];
+    var node;
+    var succes = "Fracaso";
+    stack.push(node1);
+    while(stack.length > 0) {
+        node = stack.pop();
+        console.log("->" + node.name);
+        if(node.id == goal.id){
+            succes = "Exito";
+            break;
+        }
+        if(node.visited == false) {
+            for(var i=graph.length-1; i>0; i--) {
+                if(getEdge(node, graph[i]))
+                    stack.push(graph[i]);
+            }
+        }
+        node.visited = true;
+    }
+    console.log(succes);
+};
+
+var DLS = function(graph, node1, goal, limit) {
+    var stack = [];
+    var depth_stack = [];
+    var node;
+    var depth;
+    var succes = "Fracaso";
+    stack.push(node1);
+    depth_stack.push(0);
+    while(stack.length > 0) {
+        node = stack.pop();
+        depth = depth_stack.pop();
+        console.log("->" + node.name);
+        console.log("Depth: " + depth);
+        if(node.id == goal.id){
+            succes = "Exito";
+            break;
+        }
+        if(depth < limit) {
+            if(node.visited == false) {
+                for(var i=graph.length-1; i>0; i--) {
+                    if(getEdge(node, graph[i]))
+                        stack.push(graph[i]);
+                        depth_stack.push(depth+1);
+                }
+            }
+            node.visited = true;
+        }
+    }
+    console.log(succes);
+};
+
+var getEdge = function(node, to) {
+    for(var i=0; i<node.edges.length; i++) {
+        if(node.edges[i].to == to.id){
+            return true;
+        }
+    }
+    return false;
+};
+
+
+var callDFS = function(){
+    var start;
+    var goal;
+    for(var i=0; i<logicNetwork.length; i++) {
+        if(logicNetwork[i].initial){
+            start = logicNetwork[i];
+        }
+        else if(logicNetwork[i].final){
+            goal = logicNetwork[i];
+        }
+    }
+    DFS(logicNetwork, start, goal);
+};
+
+var callDLS = function(){
+    var start;
+    var goal;
+    for(var i=0; i<logicNetwork.length; i++) {
+        if(logicNetwork[i].initial){
+            start = logicNetwork[i];
+        }
+        else if(logicNetwork[i].final){
+            goal = logicNetwork[i];
+        }
+    }
+    var limit = document.getElementById("limit").value;
+    if(limit != undefined)
+        DLS(logicNetwork, start, goal, parseInt(limit));
 };
 
 document.getElementById('files').addEventListener('change', loadGraphJSON, false);
